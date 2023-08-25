41e9ffc0b40343d022a81faaac6f3082da068e2d
nvduran <nvduran@gmail.com>
2023-08-24 21:28:24 -0500

    123

--- splitXMLin2.js
+++ splitXMLin2.js
@@ -1,55 +1,54 @@
-const fs = require('fs');
-const xml2js = require('xml2js');
+const fs = require("fs");
+const xml2js = require("xml2js");
 
 const parser = new xml2js.Parser();
 const builder = new xml2js.Builder();
 
 function splitXML(filePath) {
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.error("Error reading the file:", err);
-            return;
-        }
+        fs.readFile(filePath, (err, data) => {
+                if (err) {
+                        console.error("Error reading the file:", err);
+                        return;
+                }
 
-        parser.parseString(data, (err, result) => {
-            if (err) {
-                console.error("Error parsing the XML:", err);
-                return;
-            }
+                parser.parseString(data, (err, result) => {
+                        if (err) {
+                                console.error("Error parsing the XML:", err);
+                                return;
+                        }
 
-            const rootKeys = Object.keys(result);
-            if (rootKeys.length !== 1) {
-                console.error("The XML should have one root element.");
-                return;
-            }
+                        const rootKeys = Object.keys(result);
+                        if (rootKeys.length !== 1) {
+                                console.error("The XML should have one root element.");
+                                return;
+                        }
 
-            const rootNode = result[rootKeys[0]];
-            const children = Object.keys(rootNode);
-            const midpoint = Math.ceil(children.length / 2);
+                        const rootNode = result[rootKeys[0]];
+                        const children = Object.keys(rootNode);
+                        const midpoint = Math.ceil(children.length / 2);
 
-            const firstHalf = {};
-            const secondHalf = {};
+                        const firstHalf = {};
+                        const secondHalf = {};
 
-            firstHalf[rootKeys[0]] = {};
-            secondHalf[rootKeys[0]] = {};
+                        firstHalf[rootKeys[0]] = {};
+                        secondHalf[rootKeys[0]] = {};
 
-            for (let i = 0; i < midpoint; i++) {
-                firstHalf[rootKeys[0]][children[i]] = rootNode[children[i]];
-            }
+                        for (let i = 0; i < midpoint; i++) {
+                                firstHalf[rootKeys[0]][children[i]] = rootNode[children[i]];
+                        }
 
-            for (let i = midpoint; i < children.length; i++) {
-                secondHalf[rootKeys[0]][children[i]] = rootNode[children[i]];
-            }
+                        for (let i = midpoint; i < children.length; i++) {
+                                secondHalf[rootKeys[0]][children[i]] = rootNode[children[i]];
+                        }
 
-            const xml1 = builder.buildObject(firstHalf);
-            const xml2 = builder.buildObject(secondHalf);
+                        const xml1 = builder.buildObject(firstHalf);
+                        const xml2 = builder.buildObject(secondHalf);
 
-            fs.writeFileSync('firstHalf.xml', xml1);
-            fs.writeFileSync('secondHalf.xml', xml2);
-            console.log("XML files have been split into 'firstHalf.xml' and 'secondHalf.xml'.");
+                        fs.writeFileSync("firstHalf.xml", xml1);
+                        fs.writeFileSync("secondHalf.xml", xml2);
+                        console.log("XML files have been split into 'firstHalf.xml' and 'secondHalf.xml'.");
+                });
         });
-    });
 }
 
-// Split the XML file
-splitXML('path_to_your_input_file.xml');
+splitXML("path_to_your_input_file.xml");
